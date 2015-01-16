9fa08209549eae7b61aa408d6087acf4df366f4a
Lctseng <computerhenry321@yahoo.com.tw>
2015-01-16 22:42:47 +0800

    最初版本，可執行但效率差
    500*500：90 FPS

---
+++ Main/CLInit.h
@@ -0,0 +1,445 @@
+#include "CL/cl.h"
+#include "CL/cl_gl.h"
+#include <iostream>
+using namespace std;
+
+#define BUF_SIZE 20000
+
+cl_platform_id platform;
+cl_device_id device;
+cl_context ctx;
+cl_command_queue cmdQue;
+cl_int clErrno;
+cl_mem vert_in;
+cl_mem color_out;
+cl_mem dead_in;
+cl_mem dead_out;
+cl_program clProgram;
+cl_kernel clKernelCompute;
+cl_kernel clKernelMigrate;
+extern GLfloat* world;
+extern GLfloat* color;
+extern GLbyte* dead;
+extern size_t worldSize;
+extern size_t worldBytes;
+extern const unsigned int WORLD_HEIGHT;
+extern const unsigned int WORLD_WIDTH;
+/* CGL */
+GLuint Vbuf,Cbuf;
+
+bool checkCLGL();
+bool IsCLExtensionSupported( const char *extension );
+
+void initDevice(){
+    // get plaform
+    cerr << "Getting Platform Info..." << endl;
+    cl_uint plaNum;
+    clErrno = clGetPlatformIDs(
+        1, // max number
+        &platform, // point to store
+        &plaNum // result num
+      );
+    if(clErrno == CL_SUCCESS){
+        cerr << "CL platform ID:" << platform << endl;
+    }
+    else{
+        cerr << "Fail to get CL platform ID!" << endl;
+        exit(1);
+    }
+    // setting device
+    cerr << "Setting Device..." << endl;
+    cl_uint devNum;
+    clErrno = clGetDeviceIDs(
+        platform,
+        CL_DEVICE_TYPE_GPU,
+        1, // number of need
+        &device, // result store
+        &devNum
+      );
+
+    if(clErrno == CL_SUCCESS){
+        cerr << "CL device ID:" << device << endl;
+    }
+    else{
+        cerr << "Fail to get CL device ID!" << endl;
+        exit(1);
+    }
+
+    if(!checkCLGL()){
+        exit(1);
+    }
+
+    // create context
+    cerr << "Creating Context..." << endl;
+    cl_context_properties props[ ] =
+    {
+        CL_GL_CONTEXT_KHR, (cl_context_properties) wglGetCurrentContext( ),
+        CL_WGL_HDC_KHR, (cl_context_properties) wglGetCurrentDC( ),
+        CL_CONTEXT_PLATFORM, (cl_context_properties) platform,
+        0
+    };
+    ctx= clCreateContext(
+        props, // context properry
+        1, // number of device
+        &device, // device id
+        NULL, // callback, NONE
+        NULL, // user data, NONE
+        &clErrno
+      );
+    if(clErrno == CL_SUCCESS){
+        cerr << "CL context ID:" << ctx << endl;
+    }
+    else{
+        cerr << "Fail to create context!" << endl;
+        exit(1);
+    }
+    // create command queue
+    cerr << "Creating Command Queue..." << endl;
+    cmdQue = clCreateCommandQueue(
+        ctx,
+        device,
+        NULL, // properties
+        &clErrno
+      );
+    if(clErrno == CL_SUCCESS){
+        cerr << "CL command queue ID:" << cmdQue << endl;
+    }
+    else{
+        cerr << "Fail to create command queue!" << endl;
+        exit(1);
+    }
+     
+}
+
+bool checkCLGL(){
+    if( IsCLExtensionSupported( "cl_khr_gl_sharing" ) )
+    {
+        fprintf( stderr, "cl_khr_gl_sharing is supported.\n" );
+    }
+    else
+    {
+        fprintf( stderr, "cl_khr_gl_sharing is not supported -- sorry.\n" );
+        return false;
+    }
+    return true;
+}
+
+
+
+bool
+IsCLExtensionSupported( const char *extension )
+{
+    // see if the extension is bogus:
+    if( extension == NULL || extension[0] == '\0' )
+        return false;
+    char * where = (char *) strchr( extension, ' ' );
+    if( where != NULL )
+        return false;
+    // get the full list of extensions:
+    size_t extensionSize;
+    clGetDeviceInfo( device, CL_DEVICE_EXTENSIONS, 0, NULL, &extensionSize );
+    char *extensions = new char [ extensionSize ];
+    clGetDeviceInfo( device, CL_DEVICE_EXTENSIONS, extensionSize, extensions, NULL );
+    for( char * start = extensions ; ; )
+    {
+        where = (char *) strstr( (const char *) start, extension );
+        if( where == 0 )
+        {
+            delete [ ] extensions;
+            return false;
+    }
+    char * terminator = where + strlen(extension); // points to what should be the separator
+    if( *terminator == ' ' || *terminator == '\0' || *terminator == '\r' || *terminator == '\n' )
+    {
+        delete [ ] extensions;
+        return true;
+    }
+    start = terminator;
+    }
+}
+
+
+void initBuffer(){
+    // input data vertex
+    cerr << "Creating input data buffer...VERTEX" << endl;
+    // fill buffer
+    glGenBuffers( 1, &Vbuf );
+    glBindBuffer( GL_ARRAY_BUFFER , Vbuf);
+    glBufferData( GL_ARRAY_BUFFER, worldBytes, world,  GL_STATIC_DRAW );
+    glBindBuffer( GL_ARRAY_BUFFER , 0);
+    // bind to OpenGL
+    vert_in = clCreateFromGLBuffer( ctx, 0, Vbuf, &clErrno );
+    if(clErrno == CL_SUCCESS){
+        cerr << "Input Memory ID : " << vert_in << endl;
+    }
+    else{
+        cerr << "Fail to create input data buffer! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+    // input data dead
+    cerr << "Creating input data buffer...DEAD" << endl;
+    dead_in = clCreateBuffer(
+        ctx,
+        CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, // create read-only and copy data into it
+        worldSize * sizeof(GLbyte), // memory size
+        dead, // host pointer
+        &clErrno
+      );
+
+    if(clErrno == CL_SUCCESS){
+        cerr << "Input Memory ID : " << dead_in << endl;
+    }
+    else{
+        cerr << "Fail to create command queue! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+
+    // output data : color
+    cerr << "Creating output data buffer...COLOR" << endl;
+    // fill buffer
+    glGenBuffers( 1, &Cbuf );
+    glBindBuffer( GL_ARRAY_BUFFER , Cbuf);
+    glBufferData( GL_ARRAY_BUFFER, worldBytes, color,  GL_STATIC_DRAW );
+    glBindBuffer( GL_ARRAY_BUFFER , 0);
+    // bind to OpenGL
+    color_out = clCreateFromGLBuffer( ctx, 0, Cbuf, &clErrno );
+    if(clErrno == CL_SUCCESS){
+        cerr << "Output Memory ID : " << color_out << endl;
+    }
+    else{
+        cerr << "Fail to create output data buffer! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+
+
+    // output data dead
+    cerr << "Creating output data buffer...DEAD" << endl;
+    dead_out = clCreateBuffer(
+        ctx,
+        CL_MEM_READ_WRITE , // create read-write buffer
+        worldSize * sizeof(GLbyte), // memory size
+        NULL, // host pointer
+        &clErrno
+      );
+
+    if(clErrno == CL_SUCCESS){
+        cerr << "Output Memory ID : " << dead_out << endl;
+    }
+    else{
+        cerr << "Fail to create output DEAD buffer ! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+
+
+
+
+
+}
+
+void loadCLProgram(){
+    // create program
+    cerr << "Loading CL program source file..." << endl;
+    string prog;
+    
+    // read cl files
+    ifstream fin("GameOfLife.cl.cpp");
+    string line;
+    while(getline(fin,line)){
+        prog += line;
+        prog += "\n";
+    }
+    
+    // create cl program
+    size_t len = prog.size();
+    const char* strPtr = prog.c_str();
+    clProgram = clCreateProgramWithSource(
+        ctx,
+        1, // string count
+        &strPtr,
+        &len,
+        &clErrno
+      );
+    if(clErrno == CL_SUCCESS){
+        cerr << "CL Program ID : " << clProgram << endl;
+    }
+    else{
+        cerr << "Fail to create CL program! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+    // compile the source
+    clErrno = clBuildProgram(
+        clProgram,
+        1, // # of device
+        &device,
+        NULL, // no options,
+        NULL, // no callback
+        NULL // no user data
+      );
+    
+    if(clErrno == CL_SUCCESS){
+        cerr << "Program Build Success!" << endl;
+    }
+    else{
+        cerr << "Program Buld Fail! Errno:  " << clErrno <<endl;
+        char buf[BUF_SIZE];
+        size_t ret_size;
+        memset(buf,0x0,sizeof(char)*BUF_SIZE);
+        clErrno = clGetProgramBuildInfo(
+            clProgram,
+            device,
+            CL_PROGRAM_BUILD_LOG,
+            sizeof(char)*BUF_SIZE,
+            buf,
+            &ret_size
+          );
+        cerr << "Error Message :  "  <<endl;
+        if(ret_size >= BUF_SIZE){
+            cerr << "Error buffer not enough for CL build log" << endl;
+        }
+        else{
+            cerr << buf << endl;
+        }
+        exit(1);
+    }
+
+}
+
+void setKernel(){
+    /* ***** Compute ***** */
+    // create kernel
+    cerr << "Creating CL Compute Kernel..." << endl;
+    clKernelCompute = clCreateKernel(
+        clProgram,
+        "GenerateNextGeneration",
+        &clErrno
+      );
+    if(clErrno == CL_SUCCESS){
+        cerr << "CL Kernel ID : " << clKernelCompute  << endl;
+    }
+    else{
+        cerr << "Fail to create CL Kernel! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+    // set kernel argment
+    cerr << "Setting Kernel Argument..." << endl;
+    bool result = true;
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelCompute,0,sizeof(cl_mem),&dead_in); // kernel, arg_index , data_size, data_value
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelCompute,1,sizeof(cl_mem),&dead_out); // kernel, arg_index , data_size, data_value
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelCompute,2,sizeof(unsigned int),&WORLD_HEIGHT); // kernel, arg_index , data_size, data_value
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelCompute,3,sizeof(unsigned int),&WORLD_WIDTH); // kernel, arg_index , data_size, data_value
+    if(!result){
+        cerr << "Set kernel argment fail!" << endl;
+        exit(1);
+    }
+    else{
+        cerr << "Set kernel argment success!" << endl;
+    }
+
+
+    /* ***** Migrate ***** */
+    // create kernel
+    cerr << "Creating CL Migrate Kernel..." << endl;
+    clKernelMigrate = clCreateKernel(
+        clProgram,
+        "MigrateToNext",
+        &clErrno
+      );
+    if(clErrno == CL_SUCCESS){
+        cerr << "CL Kernel ID : " << clKernelMigrate  << endl;
+    }
+    else{
+        cerr << "Fail to create CL Kernel! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+    // set kernel argment
+    cerr << "Setting Kernel Argument..." << endl;
+    result = true;
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelMigrate,0,sizeof(cl_mem),&dead_in); // kernel, arg_index , data_size, data_value
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelMigrate,1,sizeof(cl_mem),&dead_out); // kernel, arg_index , data_size, data_value
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelMigrate,2,sizeof(cl_mem),&color_out); // kernel, arg_index , data_size, data_value
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelMigrate,3,sizeof(unsigned int),&WORLD_HEIGHT); // kernel, arg_index , data_size, data_value
+    result = result && CL_SUCCESS == clSetKernelArg(clKernelMigrate,4,sizeof(unsigned int),&WORLD_WIDTH); // kernel, arg_index , data_size, data_value
+    if(!result){
+        cerr << "Set kernel argment fail!" << endl;
+        exit(1);
+    }
+    else{
+        cerr << "Set kernel argment success!" << endl;
+    }
+}
+
+
+void runKernel(){
+    // enqueuing kernel
+    // global_size
+    size_t globalSize[2] = {WORLD_HEIGHT,WORLD_WIDTH};
+    
+    cl_event ev_compute;
+    clErrno = clEnqueueNDRangeKernel(
+        cmdQue,
+        clKernelCompute,
+        2, // work dim
+        NULL, // work offset
+        globalSize,
+        NULL, // local size
+        0, // # of wait for evnet 
+        NULL, // event list
+        &ev_compute // associated event
+      );
+    if(clErrno == CL_SUCCESS){
+        //cerr << "CL Kernel Enqueue Success!" << endl;
+    }
+    else{
+        cerr << "Fail to enqueue kernel Compute! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+    
+    
+    // migrate
+
+    // get GL buffer
+    cl_event ev_acquire;
+    clErrno = clEnqueueAcquireGLObjects( cmdQue, 1, &color_out, 0, NULL, &ev_acquire );
+    if(clErrno == CL_SUCCESS){
+        //cerr << "Acquire GL object Success!" << endl;
+    }
+    else{
+        cerr << "Fail to acquire GL object! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+    
+    cl_event ev_migrate;
+    
+    clErrno = clEnqueueNDRangeKernel(
+        cmdQue,
+        clKernelMigrate,
+        2, // work dim
+        NULL, // work offset
+        globalSize,
+        NULL, // local size
+        1, // # of wait for evnet 
+        &ev_acquire, // event list
+        &ev_migrate // associated event
+      );
+    if(clErrno == CL_SUCCESS){
+        //cerr << "CL Kernel Enqueue Success!" << endl;
+    }
+    else{
+        cerr << "Fail to enqueue kernel Migrate! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+    // release
+    cl_event ev_release;
+    clErrno = clEnqueueReleaseGLObjects( cmdQue, 1, &color_out, 1, &ev_migrate, &ev_release );
+    if(clErrno == CL_SUCCESS){
+        //cerr << "Release GL object Success!" << endl;
+    }
+    else{
+        cerr << "Fail to release GL object! Errno : " <<  clErrno <<endl;
+        exit(1);
+    }
+
+    clFinish( cmdQue );
+}
+
+
--- Main/main.cpp
+++ Main/main.cpp
@@ -8,27 +8,28 @@
 #include "glaux.h"
 #include "wglew.h"
 #include "omp.h"
+#include "view.h"
+#include "scene.h"
+#include "lights.h"
 
-
-#include "ModelInfo.h"
 #include <vector>
 #include <string>
 #include <cmath>
 #include <fstream>
-#include "CommonDisplayRoutine.h"
-#include <omp.h>
+
+#include "CLInit.h"
+
+const unsigned int WORLD_HEIGHT = 500;
+const unsigned int WORLD_WIDTH = 500;
 
 
-#define MIPMAP
-#define MULTI_THREAD
-
+#define OPEN_CL
 using namespace std;
 
-vector<mesh*> object;
 views* viewing;
 lights* lighting;
 scenes* Scene;
-vector<ModelInfo*> m_infos;
+
 int windowSize[2];
 int startX, startY;
 int oldX, oldY;
@@ -59,59 +60,36 @@ double angle_x, angle_z;
 
 GLuint v, f, p;
 
-int thread_count;
-pthread_t* threads_ptr;
-sem_t* semaphores;
-sem_t master_sem;
-pthread_mutex_t mutex;
-
-vector<int> job_list;
-
-int workload;
-
 void light(bool);
+void idle();
 void display();
 void keyboard(unsigned char, int, int);
 void mouse(int, int, int, int);
 void motion(int, int);
 
 void reshape(GLsizei , GLsizei );
+/* CL */
 
-void* monitor(void *);
 
-void idle();		// for UBW
-int time1,time2;	// for UBW
-int speed = 40;		// for UBW
+/* GOL */
+GLfloat* world;
+GLbyte* dead;
+GLfloat* color;
+unsigned int worldSize;
+unsigned int worldBytes;
+void initWorld();
+void MigrateToNext();
+void GenerateNextGeneration();
 
 int main(int argc, char** argv)
 {
-    thread_count = 0;
-#ifdef MULTI_THREAD
-    workload = 1;
-	thread_count = omp_get_num_procs()-1;
-#endif
+    // init world
+    initWorld();
+
 	viewing = new views("GameOfLife.view");
 	lighting = new lights("GameOfLife.light");
-	Scene = new scenes("scene1.scene");
+	Scene = new scenes("GameOfLife.scene");
 
-	for(unsigned int x=0;x<(Scene->scene_model.size());x++){
-		const char* model_name = (Scene->scene_model)[x].name.c_str();
-		
-		mesh* temp = new mesh(model_name);
-		object.push_back(temp);
-        m_infos.push_back(new ModelInfo(model_name,temp,thread_count));
-	}
-
-#ifdef MULTI_THREAD
-    pthread_mutex_init(&mutex, NULL);
-    threads_ptr = new pthread_t[thread_count];
-    semaphores = new sem_t[thread_count];
-    for(int i=0;i<thread_count;i++){
-        pthread_create(&threads_ptr[i],NULL,monitor,(void*) i);
-        sem_init(&semaphores[i], 0, 0);
-    }
-    sem_init(&master_sem,0,0);
-#endif
 	camera_eye[0]=viewing->eye[0];	camera_eye[1]=viewing->eye[1];	camera_eye[2]=viewing->eye[2];
 	camera_vat[0]=viewing->vat[0];	camera_vat[1]=viewing->vat[1];	camera_vat[2]=viewing->vat[2];
 	camera_vup[0]=viewing->vup[0];	camera_vup[1]=viewing->vup[1];	camera_vup[2]=viewing->vup[2];
@@ -139,16 +117,50 @@ int main(int argc, char** argv)
 	glutKeyboardFunc(keyboard);
 	glutMouseFunc(mouse);
 	glutMotionFunc(motion);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
+    //buffer
+    
+    //glPointSize( 2. );
+    
+#ifdef OPEN_CL
+    initDevice();
+    initBuffer();
+    loadCLProgram();
+    setKernel();
+#endif
 
 	glutMainLoop();
+    delete[] world;
+    delete[] dead;
+    delete[] color;
 	return 0;
 }
 
+void initWorld(){
+    worldSize = WORLD_HEIGHT * WORLD_WIDTH;
+    worldBytes = worldSize * 3 * sizeof(GLfloat);
+    world = new GLfloat[worldSize*3];
+    color = new GLfloat[worldSize*3];
+    dead = new GLbyte[worldSize];
+    for(int i=0;i<WORLD_HEIGHT;i++){
+        for(int j=0;j<WORLD_WIDTH;j++){
+            world[(i*WORLD_WIDTH+j)*3+0] = i*0.1;
+            world[(i*WORLD_WIDTH+j)*3+1] = j*0.1;
+            world[(i*WORLD_WIDTH+j)*3+2] = 10.0f;
+            color[(i*WORLD_WIDTH+j)*3+0] = 1.0f;
+            color[(i*WORLD_WIDTH+j)*3+1] = 0.5f;
+            color[(i*WORLD_WIDTH+j)*3+2] = 0.0f;
+            dead[i*WORLD_WIDTH+j] = rand()%2!=0;
+
+        }
+    }
+    cout << "World Initialized " << endl;
+}
 
 
 void display()
 {
-
 
 	// clear the buffer
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);      //清除用color
@@ -156,6 +168,8 @@ void display()
 	glEnable(GL_DEPTH_TEST);                   // Enables Depth Testing
 	glDepthFunc(GL_LEQUAL);                    // The Type Of Depth Test To Do
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//這行把畫面清成黑色並且清除z buffer
+
+    
 
 	// viewport transformation
 	glViewport(viewing->viewport[0], viewing->viewport[1], viewing->viewport[2], viewing->viewport[3]);
@@ -186,75 +200,201 @@ void display()
 
 	//注意light位置的設定，要在gluLookAt之後
 	light(0); // only ambient on 
-	
-	// draw ambient product
-    
-	// draw ambient product
-    
-	for(int x=0;x<(Scene->scene_model.size());x++){
-        auto& model_info = *m_infos[x];
-        int gl_draw_type;
-        if((x==0&&draw_ground)||!model_to_line){
-            gl_draw_type = GL_TRIANGLES;
-        }
-        else{
-            gl_draw_type = GL_POINTS;
-        }
-        glBegin(gl_draw_type);
-        for(int i=0;i < ModelInfo::WORLD_HEIGHT ;++i)
-		{	
-            for(int j=0;j < ModelInfo::WORLD_WIDTH ;++j){
-                if(!model_info.vertexList[i][j].dead){
-                    
-                    glVertex3fv(model_info.vertexList[i][j].ptr);
-			        
-                }
-            }
-            
-			
-		}
-        glEnd();
-		
-	}
 
+
+
+    
+    
+    runKernel();
+    
+    glBindBuffer( GL_ARRAY_BUFFER , Vbuf);
+    glVertexPointer( 3, GL_FLOAT, 0, (void *)0 );
+    glEnableClientState( GL_VERTEX_ARRAY );
+ 
+    glBindBuffer( GL_ARRAY_BUFFER, Cbuf );
+    glColorPointer( 3, GL_FLOAT, 0, (void *)0 );
+    glEnableClientState( GL_COLOR_ARRAY );
+    
+    
+    glDrawArrays( GL_POINTS, 0, worldSize);
+    glDisableClientState( GL_VERTEX_ARRAY );
+    glDisableClientState( GL_COLOR_ARRAY );
+    glBindBuffer( GL_ARRAY_BUFFER , 0);
     
 	glutSwapBuffers();
+    glFlush();
 }
 
 
-
-#ifdef MULTI_THREAD
-void* monitor(void* v_rank){
-    int rank = (int)v_rank;
-    printf("Create:%d , \n",rank);
-    while(true){
-        //printf("rank:%d wait for job...\n",rank);
-        sem_wait(&semaphores[rank]);
-        // job for this rank
-        m_infos[0]->ThreadGeneration(rank);
-        m_infos[0]->ThreadMigrate(rank);
-        sem_post(&master_sem);
-    }
-    pthread_exit(NULL);
-    return NULL;
-}
-#endif
 
 void idle(){
     if(move_enable){
-#ifdef MULTI_THREAD
-        for(int i=0;i<thread_count;i++){
-            sem_post(&semaphores[i]);
-        }
-        for(int i=0;i<m_infos.size();i++){
-            sem_wait(&master_sem);
-        }
-#else
-        m_infos[0]->GenerateNextGeneration();
-        m_infos[0]->MigrateToNext();
-
-#endif
-  
+        GenerateNextGeneration();
+        MigrateToNext();
         glutPostRedisplay();
+        
     }
+    
+}
+
+/*
+
+void MigrateToNext(){
+    for(int i=0;i<WORLD_HEIGHT;i++){
+        for(int j=0;j<WORLD_WIDTH;j++){
+            vertexList[i][j].dead = nextList[i][j].dead;
+        }
+    }
+}
+
+void GenerateNextGeneration(){
+    for(int i=0;i<WORLD_HEIGHT;i++){
+        for(int j=0;j<WORLD_WIDTH;j++){
+            auto& node = vertexList[i][j];
+            
+            if(node.dead){
+                int sur = Surround(i,j);
+                if(sur==3){
+                    nextList[i][j].dead = false;
+                }
+            }
+            else{
+                int sur = Surround(i,j);
+                if(sur<2){
+                    nextList[i][j].dead = true;
+                }
+                else if(sur>3){
+                    nextList[i][j].dead = true;
+                }
+            }
+            
+        }
+    }
+}
+*/
+
+void MigrateToNext(){}
+void GenerateNextGeneration(){}
+
+void light(bool only_ambient)
+{	
+	
+	for(unsigned int i=0; i<(lighting->light_source.size()); i++){
+		GLfloat no_specular[] = {0, 0, 0, 0};
+		GLfloat no_diffuse[] = {0, 0, 0, 0};
+
+		GLfloat light_specular[] = {(lighting->light_source)[i].s[0], (lighting->light_source)[i].s[1], (lighting->light_source)[i].s[2], 1.0};
+		GLfloat light_diffuse[] = {(lighting->light_source)[i].d[0], (lighting->light_source)[i].d[1], (lighting->light_source)[i].d[2], 1.0};
+		GLfloat light_ambient[] = {(lighting->light_source)[i].a[0], (lighting->light_source)[i].a[1], (lighting->light_source)[i].a[2], 1.0};
+		GLfloat light_position[] = {light_pos[0],light_pos[1],light_pos[2], 1.0};
+		glShadeModel(GL_SMOOTH);
+
+		
+		// z buffer enable
+		glEnable(GL_DEPTH_TEST);
+
+		// enable lighting
+		glEnable(GL_LIGHTING);
+		// set light property
+		glEnable(GL_LIGHT0+i);
+		glLightfv(GL_LIGHT0+i, GL_POSITION, light_position);
+		if(only_ambient){
+			glLightfv(GL_LIGHT0+i, GL_DIFFUSE, no_diffuse);
+			glLightfv(GL_LIGHT0+i, GL_SPECULAR, no_specular);
+		}
+		else{
+			glLightfv(GL_LIGHT0+i, GL_DIFFUSE, light_diffuse);
+			glLightfv(GL_LIGHT0+i, GL_SPECULAR, light_specular);
+		}
+		glLightfv(GL_LIGHT0+i, GL_AMBIENT, light_ambient);
+	}
+	GLfloat ambient[]={lighting->amb[0], lighting->amb[1], lighting->amb[2]};
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
+
+}
+
+void reshape(GLsizei w, GLsizei h)
+{
+	windowSize[0] = w;
+	windowSize[1] = h;
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	static int select = 0;
+	//printf("you press the key %c \n", key);
+	//printf("the mouse is on %lf %lf \n", x, y);
+	switch(key)
+	{
+	case 's':	// backward
+		back_x = back_x+step/20;
+		break;
+	case 'w':	// forward
+		front_x = front_x+step/20;
+		break;
+	case 'a':	// left
+		left_x = left_x+step/30;
+		break;
+	case 'd':	// right
+		right_x = right_x+step/30;
+		break;
+	case 'l':	// light movement
+		light_pos[0] =light_pos[0]-step/30;
+		break;
+	case 'j':	// light movement
+		light_pos[0] =light_pos[0]+step/30;
+		break;
+	case 'k':	// light movement
+		light_pos[2] =light_pos[2]-step/30;
+		break;
+	case 'i':	// light movement
+		light_pos[2] =light_pos[2]+step/30;
+		break;
+	case '1':
+		printf("press 1\n");
+		//select = 1;
+		break;
+	case '2':
+		printf("press 2\n");
+		//select = 2;
+		break;
+	case '3':
+		printf("press 3\n");
+		//select = 3;
+		break;
+	case 'z':    // Migrate
+        GenerateNextGeneration();
+        MigrateToNext();
+        break;
+    case 'x':
+        move_enable = !move_enable;
+        break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+    //printf("Light %.2f %.2f %2f \n", light_pos[0],light_pos[1],light_pos[2]);
+	//printf("Pos %.2f %.2f %2f %.2f \n", back_x,front_x,left_x,right_x);
+}
+void mouse(int button, int state, int x, int y){
+	if (state == GLUT_DOWN){
+		startX = x;
+		startY = y;
+		oldX = x; // without assigning oldX / oldY, vX / vY will count new x / y, which will lead rotation wrong positioning 
+		oldY = y;
+	}
+	else if(state == GLUT_UP){
+		//printf("the mouse moves %d %d \n", x-startX,  y-startY);
+	}
+}
+void motion(int x, int y){
+	//printf("the mouse is moving to %d %d \n", x, y);
+	GLdouble vX = x-oldX;
+	GLdouble vY = y-oldY;
+	rotX = rotX + vY/10; // total amount of upward / downward motion
+	rotY = rotY + vX/10; // total amount of left / right motion
+	oldX = x;
+	oldY = y;
+	glutPostRedisplay();
+    //printf("Eye %2.f %2.f\n",rotX,rotY);
 }
